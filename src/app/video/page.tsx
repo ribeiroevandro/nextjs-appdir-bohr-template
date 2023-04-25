917dfbcd6a2f0b2052a8ef90020ca1f22f36427c
Evandro Ribeiro <ribeiroevandro@live.com>
2023-04-25 13:23:27 -0300

    chore: update video

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -6,7 +6,8 @@ export default  function About() {
   return (
     <div className={styles.video}>
       <video controls autoPlay loop>
-        <source  src='/Design_sem_nome.mp4' type="video/mp4" />
+        <source  src='Design_sem_nome.mp4' type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
